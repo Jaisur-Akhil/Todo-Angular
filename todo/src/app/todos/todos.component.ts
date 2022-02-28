0ac7753caea6f16b78c4aa603ed455b3fb74de73
Akhil Jaisur <akhiljaisur19@gmail.com>
2022-03-01 01:06:58 +0530

    css + ui

--- todo/src/app/todos/todos.component.ts
+++ todo/src/app/todos/todos.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Todo} from '../models/Todo';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-
+Daily = 'Lucky to be alive '
+todos : Todo[];
   constructor() { }
 
   ngOnInit(): void {
+    this.todos = [
+      {
+        content : 'First Todo',
+        completed: false
+      },
+      {
+        content : 'Second Todo',
+        completed: true
+      }
+    ]
   }
 
 }
